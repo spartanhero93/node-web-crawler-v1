07e6d4e7e4e8c4cd090d5ff15b3976285eefd409
spartanhero93 <spartanhero93@gmail.com>
2019-02-25 21:03:25 -0500

    added rankings, popularity, total members, OP

--- animeItem.js
+++ animeItem.js
@@ -1,5 +1,5 @@
 const rp = require('request-promise')
-const url = 'https://myanimelist.net/anime/5114/Fullmetal_Alchemist__Brotherhood'
+const url = 'https://myanimelist.net/anime/32281/Kimi_no_Na_wa'
 const $ = require('cheerio')
 
 module.exports = async link => {
@@ -16,12 +16,22 @@ module.exports = async link => {
         ''
       ),
       score: $('span[itemprop=ratingValue]', html)[0].children[0].data,
+      rankingPopularityMembers: $('.di-ib.ml12.pl20.pt8', html)[0].children.map(item =>
+        item.children[1].children.map(item => item.data)
+      ),
+      /**  [0].children[1].children[0].data  */
       background: $('span[itemprop=description]', html)[0]
         .children.map(item => item.data)
         .filter(item => {
           if (item && item.length > 3) return item
         })
         .map(item => item.replace(/[\n"/]/g, '')),
+      openingThemes: $('.di-tc.va-t', html)[2]
+        .children[3].children.filter(item => {
+          if (item.name === 'span') return item
+        })
+        .map(item => item.children.map(item => item.data)),
+      coverImage: $('.js-scrollfix-bottom > div > a > img', html)[0].attribs.src,
     })
   } catch (error) {
     console.log(error)
