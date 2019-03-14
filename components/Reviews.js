81f4dc22f7979724180b36fe35ae4697dab0544f
spartanhero93 <spartanhero93@gmail.com>
2019-03-14 14:39:10 -0400

    user names fetched

--- components/Reviews.js
+++ components/Reviews.js
@@ -2,6 +2,13 @@ const $ = require('cheerio')
 
 module.exports = async objectOfReviews => {
   try {
+    /** Testing Suit */
+    // return objectOfReviews[0].children
+    //   .filter(item => item.name === 'div')[0]
+    //   .children.filter(item => item.name === 'div')[1]
+    //   .children[1].children[1].children[0].children.filter(item => item.name === 'td')[1]
+    //   .children.filter(i => i.name === 'a')[0].children[0].data
+
     const arrOfReviews = []
 
     for (const key in objectOfReviews) {
@@ -10,15 +17,25 @@ module.exports = async objectOfReviews => {
       }
     }
 
-    return arrOfReviews.map(
-      item =>
-        item.children
+    return arrOfReviews.map(item => {
+      const userObj = {
+        userName: item.children
           .filter(item => item.name === 'div')[0]
           .children.filter(item => item.name === 'div')[1]
           .children[1].children[1].children[0].children.filter(
             item => item.name === 'td'
-          )[0].children[1].children[1].children[1].attribs.src
-    )
+          )[1]
+          .children.filter(i => i.name === 'a')[0].children[0].data,
+
+        profilePic: item.children
+          .filter(item => item.name === 'div')[0]
+          .children.filter(item => item.name === 'div')[1]
+          .children[1].children[1].children[0].children.filter(
+            item => item.name === 'td'
+          )[0].children[1].children[1].children[1].attribs.src,
+      }
+      return userObj
+    })
 
     // .children
     //   .filter(item => item.name === 'div')[0]
